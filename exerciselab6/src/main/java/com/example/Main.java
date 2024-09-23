61466ea39d05d25aede5456b3dc85e281f578b32
luciaizqu <luciariolobos@gmail.com>
2024-09-23 18:21:58 +0200

    changes

--- exerciselab6/src/main/java/com/example/Main.java
+++ exerciselab6/src/main/java/com/example/Main.java
@@ -1,8 +1,11 @@
 package com.example;
-import java.time.LocalDate;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        LocalDate date = LocalDate.now();
-        System.out.println("Today is: " + date);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
     }
 }
