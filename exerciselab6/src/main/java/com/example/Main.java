9f3c333e8cad2dbbcae0758b6c016f6fcac96916
luciaizqu <luciariolobos@gmail.com>
2024-09-24 09:21:20 +0200

    task 5.2

--- exerciselab6/src/main/java/com/example/Main.java
+++ exerciselab6/src/main/java/com/example/Main.java
@@ -3,18 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Choose a greeting (1: Formal, 2: Casual)");
-        int choice = scanner.nextInt();
-
-        String greeting;
-        if (choice == 1){
-            greeting = "Good day, " + name + "!";
-        }else{
-            greeting = "Hey, " + name + "! What's up?";
-        }
-        System.out.println(greeting);
-        scanner.close();
-    }
+             System.out.print("Please enter your full name: ");
+             String name = scanner.nextLine();
+             System.out.println("Welcome to the Java World, " + name + "!");
+             scanner.close();
+         }
 }
